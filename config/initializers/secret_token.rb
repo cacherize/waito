b6e5bfcc88b7c7b285138da56d8440636a88c24e
cacherize <web.developing.maniac@gmail.com>
2014-08-05 18:05:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waito::Application.config.secret_token = '8db39c84d02938d3f950b5531a24b58920e7cfd04f464d1c002dc6723377d05954b54d6e310f26e6fca76e846e9a4e1b213cf112d752789598279d57a4279db7'
