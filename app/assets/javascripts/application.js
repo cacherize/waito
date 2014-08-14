71a08f68e531c739f6ea4a46bd2e2da71062345d
cacherize <web.developing.maniac@gmail.com>
2014-08-13 22:29:49 -0500

    added logo animation on hover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function(){
+  $("#logoLink").hover(function(){
+    $("#logo").stop().animate({bottom: '14px'});
+  }, function(){
+    $("#logo").stop().animate({bottom: 0});
+  });
+});
