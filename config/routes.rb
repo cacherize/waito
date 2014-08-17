fc943ab077ff290524ac31e24449ff1b3b49480c
cacherize <web.developing.maniac@gmail.com>
2014-08-17 00:31:58 -0500

    added judge gem and config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Waito::Application.routes.draw do
   resources :users
 
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -58,4 +57,5 @@ Waito::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  mount Judge::Engine => '/judge'
 end
