eaebf798cb3e650fbffbef9b54b5b5e370e13fd7
cacherize <web.developing.maniac@gmail.com>
2014-08-17 00:48:35 -0500

    added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
+  validates :password, presence: true, on: :create
+  validates :email, presence: true,
+    uniqueness: {case_sensitive: false},
+    format: {
+      with: /^[-0-9a-z.+_]+@[-0-9a-z.+_]+\.[a-z]{2,4}$/i,
+      if: lambda{self.email.present?}
+    },
+    on: :create
+  validates :username, presence: true,
+    uniqueness: {case_sensitive: false}, 
+    length: {minimum: 3, maximum: 30,
+      too_long:  "is too long (max 30 chars)",
+      too_short: "is too short (min 3 chars)",
+      if: lambda{self.username.present?}
+    },
+    format: {
+      with: /^[a-z0-9_-]*$/i,
+      message: 'must only contain alphanumeric characters',
+      if: lambda{self.username.present?}
+    }
 end
