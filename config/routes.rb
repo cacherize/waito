9bb44e5054280caede136edcf8d7ae9bd0db84bc
cacherize <web.developing.maniac@gmail.com>
2014-08-17 15:13:21 -0500

    deleted mounted judge resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -57,5 +57,4 @@ Waito::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  mount Judge::Engine => '/judge'
 end
