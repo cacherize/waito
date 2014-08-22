0b05f03ecabcd38604350cbfcddb5395ecc320f4
cacherize <web.developing.maniac@gmail.com>
2014-08-21 19:32:38 -0500

    added jquery email format validation

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -17,6 +17,28 @@ ClientSideValidations.callbacks.element.fail = function($element, message, callb
 ClientSideValidations.callbacks.element.pass = function($element, callback) {
   callback();
   $element.siblings('.validField').remove();
-  $element.after('<span class="validField">Success!</span');
+  $element.after('<span class="validField">That works!</span');
   // Add a success message to give the user an ego lift.
 };
+
+var userEmailValidator = function() {
+  var $el = $("#user_email");
+  $el.on('blur', function(){
+    if ($el.val() == "") {return}
+      
+    var invalid, message;
+    invalid = !/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test($el.val())
+    
+    if (!invalid) {return}
+    message = "Invalid email"
+
+    if ($el.siblings(".invalidField").length > 0) {
+      $el.siblings(".invalidField").val(failMessage);
+    } else {
+      $el.siblings('.validField').remove();
+      $el.siblings('label').wrap("<div class='field_with_errors'></div>");
+      $el.wrap("<div class='field_with_errors'></div>");
+      $el.after("<label class='message invalidField' for='user_email'>"+message+"</label>");
+    }
+  });
+}
