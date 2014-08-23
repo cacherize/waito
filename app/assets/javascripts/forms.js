d385862856d5a2d96818440dd053e5fe80a082fa
cacherize <web.developing.maniac@gmail.com>
2014-08-22 19:56:25 -0500

    removed conditionals on format validations for user and removed jquery email validation

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -20,25 +20,4 @@ ClientSideValidations.callbacks.element.pass = function($element, callback) {
   $element.after('<span class="validField">That works!</span');
   // Add a success message to give the user an ego lift.
 };
-
-var userEmailValidator = function() {
-  var $el = $("#user_email");
-  $el.on('blur', function(){
-    if ($el.val() == "") {return}
-      
-    var invalid, message;
-    invalid = !/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test($el.val())
-    
-    if (!invalid) {return}
-    message = "Invalid email"
-
-    if ($el.siblings(".invalidField").length > 0) {
-      $el.siblings(".invalidField").val(failMessage);
-    } else {
-      $el.siblings('.validField').remove();
-      $el.siblings('label').wrap("<div class='field_with_errors'></div>");
-      $el.wrap("<div class='field_with_errors'></div>");
-      $el.after("<label class='message invalidField' for='user_email'>"+message+"</label>");
-    }
-  });
-}
+=
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
   has_secure_password
   validates_confirmation_of :password
-  validates_format_of :email, with:  /^[-0-9a-z.+_]+@[-0-9a-z.+_]+\.[a-z]{2,4}$/i, if: lambda{self.email.present?}
+  validates_format_of :email, with:  /^[-0-9a-z.+_]+@[-0-9a-z.+_]+\.[a-z]{2,4}$/i
   validates_uniqueness_of :email, case_sensitive: false
   validates :password, presence: true, on: :create
   validates :email, presence: true, on: :create
   validates_length_of :username, minimum: 3, maximum: 30
   validates_length_of :password, minimum: 8, maximum: 100, if: lambda{self.password.present?}
-  validates_format_of :username, with: /^[a-z0-9_-]*$/i, message: 'must only contain alphanumeric characters', if: lambda{self.username.present?}
+  validates_format_of :username, with: /^[a-z0-9_-]*$/i, message: 'must only contain alphanumeric characters'
   validates :username, presence: true, uniqueness: {case_sensitive: false}
 end
