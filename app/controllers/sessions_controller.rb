7db680fa081a12b6f8693c5db0fd05ffc642d8cc
cacherize <web.developing.maniac@gmail.com>
2014-08-23 21:55:21 -0500

    set up sessions' controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email_or_username(params[:login])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Successfully logged in!"
+    else
+      flash.now.alert = "Invalid username/email and password combination"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Successfully logged out!'
   end
 end
