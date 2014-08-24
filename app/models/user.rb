efd2095e223627527a8b217d63b662d0877654c1
cacherize <web.developing.maniac@gmail.com>
2014-08-24 15:04:07 -0500

    rearranged user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
   has_secure_password
-  validates_confirmation_of :password
+
+  validates :email, presence: true, on: :create
   validates_format_of :email, with:  /^[-0-9a-z.+_]+@[-0-9a-z.+_]+\.[a-z]{2,4}$/i
   validates_uniqueness_of :email, case_sensitive: false
-  validates :password, presence: true, on: :create
-  validates :email, presence: true, on: :create
-  validates_length_of :username, minimum: 3, maximum: 30
-  validates_length_of :password, minimum: 8, maximum: 100, if: lambda{self.password.present?}
-  validates_format_of :username, with: /^[a-z0-9_-]*$/i, message: 'must only contain alphanumeric characters'
   validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates_length_of :username, minimum: 3, maximum: 30
+  validates_format_of :username, with: /^[a-z0-9_-]*$/i, message: 'must only contain alphanumeric characters'
+  validates :password, presence: true, on: :create
+  validates_length_of :password, minimum: 8, maximum: 100, if: lambda{self.password.present?}
 
   def self.username_search(arg)
     where('lower(username) = ?', arg.downcase).first
