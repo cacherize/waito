87a3b71b9d78f27a0822bdddea5218c5b424e760
cacherize <web.developing.maniac@gmail.com>
2014-08-24 19:02:05 -0500

    set up create action of password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,6 +3,15 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email_or_username(params[:login])
+    
+    if user
+      user.reset_password
+    end
+
+    respond_to do |format|
+      format.html{redirect_to root_path, notice: "Check your inbox for a password with reset instructions."}
+    end
   end
 
   def edit
