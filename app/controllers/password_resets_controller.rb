020b200dd0025505c9dcdcb74cbd9db0af8bd81f
cacherize <web.developing.maniac@gmail.com>
2014-08-24 22:27:27 -0500

    set up update action in password resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -19,5 +19,25 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+    @user = User.find_by_password_reset_token(params[:id])
+
+    if params[:user]
+      password_attrs = params[:user].keep_if{|k,v| ["password", "password_confirmation"].include?(k)}
+
+      unless password_attrs.any?{|k, v| v.present?}
+        password_attrs = nil
+        @user.errors.add(:password, 'is required')
+      end
+    else
+      @user.errors.add(:password, 'is required')
+    end
+
+    respond_to do |format|
+      if password_attrs && @user.update_attributes(params[:user])
+        format.html{redirect_to login_path, notice: 'Success! Password has been reset!'}
+      else
+        format.html{render :edit}
+      end
+    end
   end
 end
