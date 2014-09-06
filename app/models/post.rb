e1fc59283cbeba71498c6a1eb7f887c75d460bd4
cacherize <web.developing.maniac@gmail.com>
2014-09-06 18:12:38 -0500

    added :tag_list virtual attribute and blank setter and getter methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :description, :archived_at
+  attr_accessible :title, :description, :archived_at, :tag_list
+
+  def tag_list=(values)
+  end
+
+  def tag_list
+  end
 end
