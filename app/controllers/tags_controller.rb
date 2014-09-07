b4a2019a84ecf436a9219c8c60165284f26fea0a
cacherize <web.developing.maniac@gmail.com>
2014-09-06 20:03:17 -0500

    set up create and new actions for tag controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,9 +3,19 @@ class TagsController < ApplicationController
   end
 
   def new
+    @tag = Tag.new
   end
 
   def create
+    @tag = Tag.new(params[:tag])
+
+    respond_to do |format|
+      if @tag.save
+        format.html{redirect_to tags_path, notice: "Successfully added tag!"}
+      else
+        format.html{render :new}
+      end
+    end
   end
 
   def edit
