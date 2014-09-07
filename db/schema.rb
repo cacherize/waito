3a985f2ebce9bacbcb4a0f4a692a80420abe8983
cacherize <web.developing.maniac@gmail.com>
2014-09-06 20:20:57 -0500

    added post_tags table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140907002607) do
+ActiveRecord::Schema.define(:version => 20140907011715) do
+
+  create_table "post_tags", :force => true do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "post_tags", ["post_id", "tag_id"], :name => "index_post_tags_on_post_id_and_tag_id", :unique => true
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
