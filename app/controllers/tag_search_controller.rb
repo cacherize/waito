613aab0628cc08a29df825007368370899fcf812
cacherize <web.developing.maniac@gmail.com>
2014-09-07 22:05:14 -0500

    added show action for tag search controller

--- app/controllers/tag_search_controller.rb
+++ app/controllers/tag_search_controller.rb
@@ -1,8 +1,14 @@
 class TagSearchController < ApplicationController
   def index
     return unless params[:term]
-
     results = Tag.where(['lower(name) LIKE ?', "%#{params[:term].downcase}%"])
+
+    render :json => results.to_json(:only => [ :id, :name ]), :callback => params['callback']
+  end
+
+  def show
+    return unless params[:tag_ids]
+    results = Tag.find(params[:tag_ids])
 
     render :json => results.to_json(:only => [ :id, :name ]), :callback => params['callback']
   end
