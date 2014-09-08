d687f8eafa2eff0e376c7045a3ff51dd705dd033
cacherize <web.developing.maniac@gmail.com>
2014-09-07 22:47:35 -0500

    setup :tag_list virtual attribute setter method and added validation for post :title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :description, :archived_at, :tag_list
 
-  has_many :post_tags
+  has_many :post_tags, autosave: true
   has_many :tags, through: :post_tags
 
-  def tag_list=(values)
-  end
+  validates :title, presence: true, length: {maximum: 325}
 
-  def tag_list
+  def tag_list=(values)
+    self.tag_ids = values.to_s.split(",")
   end
 end
