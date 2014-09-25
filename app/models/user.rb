42706768b206e739308b76f13a8da513ac269c75
cacherize <web.developing.maniac@gmail.com>
2014-09-24 23:58:47 -0500

    added :user_id attribute to attr_accessible for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation, :user_id
   has_secure_password
 
   has_many :posts
