42a5e3a75dc2599c16a6b0979acfa8a0a444140c
cacherize <web.developing.maniac@gmail.com>
2014-09-27 17:12:27 -0500

    added reputation class method to create or update a reputation

--- app/models/reputation.rb
+++ app/models/reputation.rb
@@ -2,4 +2,16 @@ class Reputation < ActiveRecord::Base
   attr_accessible :user_id, :value
   belongs_to :reputable, polymorphic: true
   belongs_to :user
+
+  def self.create_or_update_rep object, user, value
+    rep = object.reputations.where(user_id: user.id).first
+
+    unless rep
+      rep = object.reputations.new(user_id: user.id)
+    end
+
+    rep.value = value
+
+    return rep
+  end
 end
