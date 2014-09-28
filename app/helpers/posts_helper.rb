0bbb07e190be1138d69fbdf5f7e0e0222edb8794
cacherize <web.developing.maniac@gmail.com>
2014-09-27 22:34:30 -0500

    added helper for voting link

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,15 @@ module PostsHelper
       post_path(post)
     end
   end
+
+  def voting_link(value, post, existing_vote)
+    path = post_vote_path(post, rep_val: value)
+    css_classes = value > 0 ? "like" : "dislike"
+
+    if existing_vote && existing_vote.value == value
+      css_classes += " voted"
+    end
+
+    link_to '', path, method: :put, class: css_classes
+  end
 end
