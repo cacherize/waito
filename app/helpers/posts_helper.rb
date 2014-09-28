3e1e2a5289e54924da3e1e9560e478240775c570
cacherize <web.developing.maniac@gmail.com>
2014-09-28 18:19:49 -0500

    added title attribute to voting link on post show

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -9,12 +9,15 @@ module PostsHelper
 
   def voting_link(value, post, existing_vote)
     path = post_vote_path(post, rep_val: value)
-    css_classes = value > 0 ? "like" : "dislike"
+    upvote = value > 0
+    css_classes = upvote ? "like" : "dislike"
+    title = upvote ? '+1' : '-1'
 
     if existing_vote && existing_vote.value == value
       css_classes += " voted"
+      title += ' (voted)'
     end
 
-    link_to '', path, method: :put, class: css_classes
+    link_to '', path, method: :put, class: css_classes, title: title
   end
 end
