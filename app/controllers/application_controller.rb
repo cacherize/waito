cd9dfb00303b4a128994c258760f5a4a925e801c
cacherize <web.developing.maniac@gmail.com>
2014-09-28 20:43:01 -0500

    added application controller methods to authenticate user and to return to previous location after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :store_path
+
+  def store_path
+    session[:return_to] = request.fullpath
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
   private
+
+  def authenticate_user
+    redirect_to login_path, alert: "Please login before completing that action." unless current_user
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
