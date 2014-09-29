0aa6f2efd6cf0a1fc8915157a71ee3d4d5770755
cacherize <web.developing.maniac@gmail.com>
2014-09-28 23:19:06 -0500

    add id attribute to popup helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,8 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, options).render(text).html_safe
   end
 
-  def popup(partial_path)
-    content_tag :div, class: 'popup' do
+  def popup(partial_path, id='popup')
+    content_tag :div, class: 'popup', id: id do
       content_tag :div, class: 'backdrop' do
         content_tag :div, class: 'box' do
           render partial_path
