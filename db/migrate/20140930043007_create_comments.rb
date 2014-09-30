0d0ec2ba942ec7752fe8b9fe7c7f0792d0759c3a
cacherize <web.developing.maniac@gmail.com>
2014-09-30 17:54:12 -0500

    set up migration for creating comments table and ran migration

--- db/migrate/20140930043007_create_comments.rb
+++ db/migrate/20140930043007_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+      t.belongs_to :user
+      t.text :message
+      t.datetime :deleted_at
+      
       t.timestamps
     end
   end
