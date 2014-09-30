2df879e0026a78d69f6c7f7c1ccc7d529523ebd9
cacherize <web.developing.maniac@gmail.com>
2014-09-30 18:46:20 -0500

    set up comment controller create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def create
+    load_commentable
+    @comment = @commentable.comments.new(params[:comment])
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html{redirect_to request.referer, notice: "Success! Added comment!"}
+      else
+        format.html{redirect_to request.referer, alert: 'An error occurred. Please try again.'}
+      end
+    end
+  end
+
+private
+
+  def load_commentable
+    resource, id = request.path.split('/')[1, 2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
 end
