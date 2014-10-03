bd4c5a81f4e4c8ef2f4ae6686c36eb00a070ba08
cacherize <web.developing.maniac@gmail.com>
2014-10-02 21:44:43 -0500

    fixed typo in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Waito::Application.routes.draw do
   resources :comments, only: :destroy
-  get 'comments/:comment_id/reply_form', to: 'comment#new', as: 'load_reply_form'
+  get 'comments/:comment_id/reply_form', to: 'comments#new', as: 'load_reply_form'
   post 'comments/:comment_id/reply', to: 'comments#create', as: 'comment_reply'
   put 'posts/:post_id/vote', to: 'reputations#update', as: 'post_vote'
   get 'tag_add', to: 'tag_search#show', as: 'tag_add'
