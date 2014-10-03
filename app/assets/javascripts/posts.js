32f76c24a31e895642b9157003e5ad18103d28f5
cacherize <web.developing.maniac@gmail.com>
2014-10-02 23:30:32 -0500

    moved js on post#show to post.js file

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,21 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  $(".sortMenu").sortMenu();
+  $("#commentList").on('click', '.replyFormLoadLink', function(event){
+    var $that = $(this),
+        $constraint = $that.closest('.postComment'),
+        $container = $that.closest("#commentList"),
+        $rel = $that.attr('rel');
+
+    $container.find(".subCommentForm").hide();
+
+    if ($("#commentForm_"+$rel).length > 0) {
+      $("#commentForm_"+$rel).slideDown();
+    } else {
+      $.get(this.href, function(data){
+        $constraint.after(data);
+      });
+    }
+
+    event.preventDefault();
+  });
+});
