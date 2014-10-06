acfb5de77c48568efc033f1bb2eed07b3983242e
cacherize <web.developing.maniac@gmail.com>
2014-10-05 20:49:04 -0500

    added associations for comment reputations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :user_id, :message
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+  has_many :reputations, as: :reputable
   has_many :comments, as: :commentable
 
   SORT_OPTIONS = {
