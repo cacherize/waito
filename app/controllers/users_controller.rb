9bf91841439ac55d774e99b6f87f0537514db0f8
cacherize <web.developing.maniac@gmail.com>
2014-10-26 21:03:20 -0500

    load presigned post in user#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{Date.today.year}/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
     @user = User.username_search(params[:id])
   end
 
