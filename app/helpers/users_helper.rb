7a2bcaa7f774c39586fcd45a7a9b02f5d0a7997f
cacherize <web.developing.maniac@gmail.com>
2014-11-09 19:42:34 -0600

    set up helper method for displaying user avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def user_med_avatar(user)
+    content_tag :div, class: 'userImageMedWrapper' do
+      link_to_if user, image_tag(user.avatar_url, class: 'userImageMed'), user
+    end
+  end
+
+  def user_lg_avatar(user)
+    content_tag :div, class: 'userImageWrapper' do
+      link_to_if user, image_tag(user.avatar_url, class: 'userImage'), user
+    end
+  end
 end
