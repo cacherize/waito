d488c5bf2b0e1a24d23e568fd43ef105a31af25a
cacherize <web.developing.maniac@gmail.com>
2014-11-09 23:31:42 -0600

    added constant for list of support ticket reasons

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -1,3 +1,10 @@
 class SupportTicket < ActiveRecord::Base
   attr_accessible :email, :message, :reason, :user_id
+
+  TICKET_REASONS = {
+    "General Comment" => "general comment",
+    "Unable to Sign Up" => "cant signup",
+    "Report Bug" => "bug",
+    "Other" => "other"
+  }
 end
