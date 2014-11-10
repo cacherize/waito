7e2af8ab9fb22cc7d58a685a900a55150f0d6f16
cacherize <web.developing.maniac@gmail.com>
2014-11-10 00:00:14 -0600

    added validations to support ticket model

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -1,6 +1,21 @@
 class SupportTicket < ActiveRecord::Base
   attr_accessible :email, :message, :reason, :user_id
 
+  validates :email, 
+    presence: true, 
+    format: {
+      with:  /^[-0-9a-z.+_]+@[-0-9a-z.+_]+\.[a-z]{2,4}$/i, 
+      if: lambda{self.email.present?}
+    }
+  validates :message, 
+    presence: true, 
+    length: {
+      minimum: 5,
+      maximum: 1000,
+      if: lambda{self.message.present?}
+    }
+  validates :reason, presence: true
+
   TICKET_REASONS = {
     "General Comment" => "general comment",
     "Unable to Sign Up" => "cant signup",
