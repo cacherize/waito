8b527932f6f13bcd1e4988c1c4fc13bf4d27304e
cacherize <web.developing.maniac@gmail.com>
2014-11-10 23:58:47 -0600

    added alt attribute to user helper avatar methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,15 @@
 module UsersHelper
   def user_med_avatar(user)
     content_tag :div, class: 'userImageMedWrapper' do
-      link_to_if user, image_tag(user.avatar_url, class: 'userImageMed'), user
+      link_to_if user, 
+        image_tag(user.avatar_url, class: 'userImageMed', alt: "#{user.username}'s avatar"), user
     end
   end
 
   def user_lg_avatar(user)
     content_tag :div, class: 'userImageWrapper' do
-      link_to_if user, image_tag(user.avatar_url, class: 'userImage'), user
+      link_to_if user, 
+        image_tag(user.avatar_url, class: 'userImage', alt: "#{user.username}'s avatar"), user
     end
   end
 end
