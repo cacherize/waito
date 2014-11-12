39df3244267bd8159a72c2f57351764dc82c7f45
cacherize <web.developing.maniac@gmail.com>
2014-11-11 22:31:45 -0600

    validate previously submitted tickets, restrict to submitting one every 5 minutes

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -17,6 +17,16 @@ class SupportTicket < ActiveRecord::Base
       if: lambda{self.message.present?}
     }
   validates :reason, presence: true
+  validate :past_resubmission_period
+
+  def past_resubmission_period
+    last_ticket = SupportTicket.where(public_address: self.public_address).order('created_at ASC').last
+
+    if last_ticket && last_ticket.created_at > 5.minutes.ago
+      timer = ((last_ticket.created_at - 5.minutes.ago)/60).ceil
+      errors.add(:base, "You already submitted a ticket. Please wait #{timer} minute#{'s' unless timer == 1} before submitting another one.")
+    end
+  end
 
   TICKET_REASONS = {
     "General Comment" => "general comment",
