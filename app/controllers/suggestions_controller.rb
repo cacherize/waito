800cb0a03b01ab626f348e1417ca421011070e5b
cacherize <web.developing.maniac@gmail.com>
2014-11-12 19:54:34 -0600

    added each action to suggestions controller

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,19 @@
 class SuggestionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
 end
