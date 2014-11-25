b85e589f9096a82c115cb73731fc4f84e0f2e408
cacherize <web.developing.maniac@gmail.com>
2014-11-24 21:32:33 -0600

    set up create action in suggestions controller

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -7,6 +7,15 @@ class SuggestionsController < ApplicationController
   end
 
   def create
+    @suggestion = current_user.suggestions.new(params[:suggestion])
+    
+    respond_to do |format|
+      if @suggestion.save
+        format.html{redirect_to suggestions_path, notice: 'Success! Thank you for your suggestion!'}
+      else
+        format.html{render :new}
+      end
+    end
   end
 
   def edit
