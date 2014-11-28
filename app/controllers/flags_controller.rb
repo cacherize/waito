7d9fb5d3a803ee4d379fff1abf569e5f3a0ca9a3
cacherize <web.developing.maniac@gmail.com>
2014-11-28 02:23:33 -0600

    set up create action of flags controller

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,7 +1,24 @@
 class FlagsController < ApplicationController
   def create
+    load_flaggable
+    @flag = @flaggable.flags.new(params[:flag].merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @flag.save
+        format.html{redirect_to @flaggable, notice: 'Thank you for the feedback!'}
+      else
+        format.html{redirect_to @flaggable, alert: DEFAULT_ERROR_MSG}
+      end
+    end
   end
 
   def destroy
+  end
+
+private
+
+  def load_flaggable
+    resource, id = request.path.split('/')[1, 2]
+    @flaggable = resource.singularize.classify.constantize.find(id)
   end
 end
