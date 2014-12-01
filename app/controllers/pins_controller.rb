b1983e7e820fcf4ec61161417eff7fab07c2524c
cacherize <web.developing.maniac@gmail.com>
2014-11-30 21:47:15 -0600

    set up create and destroy action for pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,26 @@
 class PinsController < ApplicationController
+  def create
+    @pin = Pin.new(params[:pin])
+
+    respond_to do |format|
+      if @pin.save
+        format.html{redirect_to @pin.post, notice: 'Success! Post pinned.'}
+      else
+        format.html{redirect_to @pin.post, alert: DEFAULT_ERROR_MSG}
+      end
+    end
+  end
+
+  def destroy
+    @pin = Pin.find(params[:id])
+
+    respond_to do |format|
+      if @pin.destroy
+        format.html{redirect_to @pin.post, notice: 'Success! Post unpinned.'}
+      else
+        format.html{redirect_to @pin.post, alert: DEFAULT_ERROR_MSG}
+      end
+    end
+  end
 end
+
