7340b473fd72f30cff08fc77338b445d3ae28cb9
cacherize <web.developing.maniac@gmail.com>
2014-12-08 22:10:52 -0600

    set up controller actions

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,10 +1,29 @@
 class FaqsController < ApplicationController
   def index
+    @faqs = Faq.all
   end
 
   def update
+    @faq = Faq.find(params[:id])
+
+    respond_to do |format|
+      if @faq.update_attributes(params[:faq])
+        format.html {render :index}
+      else
+        format.html {redirect_to faqs_path, notice: "Success! Updated FAQ!"}
+      end
+    end
   end
 
   def create
+    @faq = Faq.new(params[:faq])
+
+    respond_to do |format|  
+      if @faq.save
+        format.html {render :index}
+      else
+        format.html {redirect_to faqs_path, notice: "Success! Added FAQ!"}
+      end
+    end
   end
 end
